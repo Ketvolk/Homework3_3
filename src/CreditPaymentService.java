430cbf80a9eb30c68bdb291a5bb35eae9a001975
Ekaterina <ketvolk@mail.ru>
2023-04-22 23:25:59 +0300

    Initial commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,11 @@
+public class CreditPaymentService {
+    public int calculate (int creditSum, int periodYear, double percent) {
+        double result = creditSum * (((Math.pow ((1 + percent/100/12),periodYear*12)) * percent/100/12) / ((Math.pow ((1 + percent/100/12),periodYear*12)) - 1));
+        System.out.println();
+        System.out.println("Сумма кредита, руб.: " + creditSum);
+        System.out.println("Сумма кредита, лет: " + periodYear);
+        System.out.println("Процент: " + percent + "%");
+        return (int) result;
+    }
+
+}

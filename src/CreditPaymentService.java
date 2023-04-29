b3c857b5b315482bd781920ff585502a51518396
Ekaterina <ketvolk@mail.ru>
2023-04-29 12:31:36 +0300

    CodeFormat changed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
-    public int calculate (int creditSum, int periodYear, double percent) {
-        double result = creditSum * (((Math.pow ((1 + percent/100/12),periodYear*12)) * percent/100/12) / ((Math.pow ((1 + percent/100/12),periodYear*12)) - 1));
+    public int calculate(int creditSum, int periodYear, double percent) {
+        double result = creditSum * (((Math.pow((1 + percent / 100 / 12), periodYear * 12)) * percent / 100 / 12) / ((Math.pow((1 + percent / 100 / 12), periodYear * 12)) - 1));
         System.out.println();
         System.out.println("Сумма кредита, руб.: " + creditSum);
         System.out.println("Сумма кредита, лет: " + periodYear);
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int payment1 = service.calculate (1_000_000, 1, 9.99);
+        int payment1 = service.calculate(1_000_000, 1, 9.99);
         System.out.println("    Платеж в месяц, р.: " + payment1);
 
-        int payment2 = service.calculate (1_000_000, 2, 9.99);
+        int payment2 = service.calculate(1_000_000, 2, 9.99);
         System.out.println("    Платеж в месяц, р.: " + payment2);
 
-        int payment3 = service.calculate (1_000_000, 3, 9.99);
+        int payment3 = service.calculate(1_000_000, 3, 9.99);
         System.out.println("    Платеж в месяц, р.: " + payment3);
     }
 
-    }
+}
 
 
